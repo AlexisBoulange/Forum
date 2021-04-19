6a83a42b438f44a60a562882deb2d10ea63ba099
ZiCkYS <alexisboulange88@gmail.com>
2021-04-19 20:20:21 +0200

    Create UserManager.php

---
+++ model/manager/UserManager.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Model\Manager;
+
+use App\AbstractManager;
+
+class UserManager extends AbstractManager
+{
+
+    private static $classname = "Model\Entity\User"; //Fully qualified classname
+
+
+    public function __construct(){
+        self::connect(self::$classname);
+    }
+    
+
+
+
+
+
+    public function findAll(){                      //fonction pour trouver toutes les catégories
+
+        $sql = "SELECT * FROM user";                //On SELECT tout les éléments de la table category
+
+        return self::getResults(
+            self::select($sql, null, true), 
+            self::$classname
+        );
+    }
+
+    public function findOneById($id){
+        $sql = "SELECT *
+                FROM user
+                WHERE id_user = :id";
+
+        return self::getOneOrNullResult(
+            self::select($sql,
+                        ["id" =>$id],
+                        false),
+            self::$classname
+        );
+    }
+}
