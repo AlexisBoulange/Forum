1205e9cc95ca30d03ff725c3e0d87e3deee07fa2
ZiCkYS <alexisboulange88@gmail.com>
2021-04-21 18:25:32 +0200

    Update MessageManager.php

--- model/manager/MessageManager.php
+++ model/manager/MessageManager.php
@@ -16,15 +16,41 @@ class MessageManager extends AbstractManager
     
 
 
-
-
-
     public function findAll(){                      //fonction pour trouver toutes les catégories
 
         $sql = "SELECT * FROM message";             //On SELECT tout les éléments de la table category
 
         return self::getResults(
-            self::select($sql, null, true), 
+            self::select($sql, null, false), 
+            self::$classname
+        );
+    }
+
+    public function findOneById($id){
+        $sql = "SELECT *
+                FROM message
+                WHERE id_message = :id";
+
+        return self::getOneOrNullResult(
+            self::select($sql,
+                        ["id" =>$id],
+                        false),
+        self::$classname
+        );
+    }
+
+    public function findMessagesByTopic($id){
+
+        $sql = "SELECT m.id_message, m.text, m.creationDate, m.topic_id, t.title
+                FROM message m INNER JOIN topic t
+                ON m.topic_id = t.id_topic
+                WHERE t.id_topic = :id
+                ORDER BY m.creationDate ASC";
+
+        return self::getResults(
+            self::select($sql,
+            ["id" => $id],
+            true),
             self::$classname
         );
     }
