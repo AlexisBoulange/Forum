6093c80509c68434e316766dd53facdd5cb13bbe
ZiCkYS <alexisboulange88@gmail.com>
2021-04-21 18:25:48 +0200

    Update listCategories.php

--- view/listCategories.php
+++ view/listCategories.php
@@ -1,15 +1,21 @@
-<h1>Liste des catégories</h1>
-
-<ul>
 <?php
-foreach($data['categories'] as $category){
-    ?>
-    <li><?php $name = $category->getName();
-        $id = $category->getId();?>
-    
-    <a href="?ctrl=category&method=listTopicsByCategory&id=<?=$id?>"><?= $name ?></a> 
-    </li>
+$nb = $data['nb'];
+var_dump($data['nb']);
+var_dump($data['categories']);
+var_dump($data['topics'])
+?>
 
+<h1>Il y a <?= count($data['categories']);?> catégories</h1>
 
 <?php
-}?></ul>
+    foreach($data['categories'] as $category){
+        $name = $category->getName();
+        $id = $category->getId();
+        $nb = $category->getNb();
+?>
+
+    <article class="lists">
+        <a class="topic" href="?ctrl=category&method=listTopicsByCategory&id=<?=$id?>"><?= $name ?></a> <span><?=$nb;?></span>
+    </article>
+    <?php
+}?>
