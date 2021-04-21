bed60088dab4bb884fd3f3fdf5e72e294a75b31e
ZiCkYS <alexisboulange88@gmail.com>
2021-04-21 18:25:55 +0200

    Update listTopics.php

--- view/listTopics.php
+++ view/listTopics.php
@@ -1,13 +1,14 @@
-<h1>Liste des topics</h1>
+<h1>Il y a <?= count($data['topics']);?> topics </h1>
 
-<ul>
+
 <?php
 foreach($data['topics'] as $topic){
     ?>
-    <article class="lists">
-        <p><?= $topic->getTitle() ?> <a class="btn btn-dark" href="#"> Modifier</a><a class="btn btn-danger" href="#"> Supprimer</a></p>
+    <article class="lists row align-content-md-center align-items-center">
+        <a class="mr-auto p-2 topic justify-content-center" href="?ctrl=topic&method=listMessagesByTopic&id=<?= $topic->getId() ?>"><?= $topic->getTitle() ?></a>
+        <a class="btn btn-success" href="#"> Modifier</a>
+        <a class="btn btn-danger" href="#"> Supprimer</a>
     </article>
     <?php
 }?>
     
-</ul>
