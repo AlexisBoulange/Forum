5b922e4ce7a56a2e0fe102c7e2602f42be572e1f
ZiCkYS <alexisboulange88@gmail.com>
2021-04-21 18:25:26 +0200

    Update TopicController.php

--- controller/TopicController.php
+++ controller/TopicController.php
@@ -3,6 +3,7 @@
 namespace Controller;
 
 use Model\Manager\TopicManager;
+use Model\Manager\MessageManager;
 
 class TopicController {
 
@@ -15,6 +16,29 @@ class TopicController {
             "view" => "listTopics.php",
             "data" => [
                 "topics" => $topics,
+            ]
+        ];;
+    }
+
+    public function listMessagesByTopic(){
+
+        $id = (isset($_GET['id'])) ? $_GET['id'] : null; // On récupère l'id 
+
+        //Je met mes classes dans des var que j'appellerai ensuite
+        $messageModel = new MessageManager;
+        $topicModel = new TopicManager;
+        
+        //J'utilise la méthode findOneById() de TopicManager
+        $topics = $topicModel->findOneById($id);
+        //J'utilise la méthode findTopicsByCategory() de MessageManager
+        $messages = $messageModel->findMessagesByTopic($id);
+
+
+        return [
+            "view" => "listMessagesByTopic.php",
+            "data" => [
+                "topics" => $topics,
+                "messages" => $messages,
             ]
         ];;
     }
