d083d8598030cf6b9329b9ee763f9ebe3b4c2307
ZiCkYS <alexisboulange88@gmail.com>
2021-04-26 00:14:49 +0200

    Create login.php

---
+++ view/security/login.php
@@ -0,0 +1,19 @@
+
+<h1>Connectez-vous</h1>
+
+<form action="?ctrl=security&method=login" method="post">
+    <div class="form-group">
+        <label for="username">Identifiant</label>
+        <input type="text" class="form-control" name="username" id="username" placeholder="Pseudonyme">
+    </div>
+    <div class="form-group">
+        <label for="password">Mot de passe</label>
+        <input type="password" class="form-control" name="password" id="password" placeholder="">
+    </div>
+    <!-- <div class="form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+        <label class="form-check-label" for="exampleCheck1">Se rappeler de moi</label>
+    </div> -->
+    <button type="submit" class="btn btn-primary">Se connecter</button>
+    
+</form>
