75014cf5f73a8e70be841499590535ab39c82cde
ZiCkYS <alexisboulange88@gmail.com>
2021-04-26 00:14:55 +0200

    Create listTopics.php

---
+++ view/topic/listTopics.php
@@ -0,0 +1,14 @@
+<h1>Il y a <?= count($data['topics']);?> topics </h1>
+
+
+<?php
+foreach($data['topics'] as $topic){
+    ?>
+    <article class="lists row align-content-md-center align-items-center">
+        <a class="mr-auto p-2 topic " href="?ctrl=topic&method=listMessagesByTopic&id=<?= $topic->getId() ?>"><?= $topic->getTitle() ?></a>
+        <a class="btn btn-success" href="#"> Modifier</a>
+        <a class="btn btn-danger" href="#"> Supprimer</a>
+    </article>
+    <?php
+}?>
+    <a class="btn btn-primary" href="?ctrl=topic&method=createTopic&method=categoriesListForm"> Créer un nouveau sujet</a>
