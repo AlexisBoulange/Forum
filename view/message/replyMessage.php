a5a46217c183f8253ce9395f058683816d676714
ZiCkYS <alexisboulange88@gmail.com>
2021-04-26 00:14:46 +0200

    Create replyMessage.php

---
+++ view/message/replyMessage.php
@@ -0,0 +1,9 @@
+<form action="?ctrl=message&method=replyMessage" method="post">
+
+    <div class="form-group">
+        <label for="text">Example textarea</label>
+        <textarea class="form-control" name="text" id="text" rows="3"></textarea>
+    </div>
+<button type="submit" class="btn btn-primary">Valider</button>
+
+</form>
